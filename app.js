d697f2439b6b0fdac8999a476f73c4f5d93c1f48
Tobias Ocula <tobias.ocula@outlook.com>
2025-07-03 21:08:13 +0200

    backend changes

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const ethers = require("ethers");
 
 const tokenAddress = "0xb8b7Fd8003d0c975694F4c7A348a2946fEE4E33B";
 
-app.get("/", async (req, res) => {
+app.get("/rebase", async (req, res) => {
   
   const provider = new ethers.JsonRpcProvider(process.env.ALCHEMY_URL);
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
