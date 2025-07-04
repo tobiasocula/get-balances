8b3d97ab97ddfb6d4b9fcdf2a55286080e281a98
Tobias Ocula <tobias.ocula@outlook.com>
2025-07-04 09:23:28 +0200

    added logice

--- app.js
+++ app.js
@@ -9,19 +9,40 @@ const ethers = require("ethers");
 const tokenAddress = "0xb8b7Fd8003d0c975694F4c7A348a2946fEE4E33B";
 
 app.get("/rebase", async (req, res) => {
+
+  // parameters: 
+  // new supply / demand ratio
+  // demo accounts
+  const accounts = req.query.accounts;
+  const ratio = req.query.ratio;
   
   const provider = new ethers.JsonRpcProvider(process.env.ALCHEMY_URL);
   const wallet = new ethers.Wallet(process.env.PRIVATE_KEY, provider);
   const contract = new ethers.Contract(tokenAddress, artifact.abi, wallet);
 
-
+  const supply = await contract.totalSupply();
+  const newSupply = supply * (ratio - 1);
+  if (newSupply > supply) {
+    const diff = newSupply - supply;
+    const mintPerAccount = diff / (accounts.length + 1);
+    for (const acc of accounts) {
+      await contract.mintTo(acc, mintPerAccount);
+    }
+  }
+  
+  res.json({'result': true})
 
 });
 
 app.get('/balances', async (req, res) => {
+  // parameter: accountss (array of addresses)
   const provider = new ethers.JsonRpcProvider(process.env.ALCHEMY_URL);
-  const balance = await provider.getBalance(account);
-  res.json({'balance': balance});
+  let result = [];
+  for (const acc of req.query.accounts) {
+    result.push(await provider.getBalance(acc));
+  }
+  
+  res.json({'balances': result});
 })
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
